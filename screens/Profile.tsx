9d5861ddb4e9c2634e11abea7b5a523eae953996
Mousa Adolai <mousa.adolai@gmail.com>
2020-08-05 07:01:54 -0400

    made general components for profile page

--- screens/Profile.tsx
+++ screens/Profile.tsx
@@ -1,47 +1,34 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
-import VisualPostButton from '../components/VisualPostButton';
-import AudioPostButton from '../components/AudioPostButton'
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet, 
+    View
+} from 'react-native';
 
+import Header    from '../components/Header';
+import Bar       from '../components/Bar';
+import PhotoGrid from '../components/PhotoGrid';
 
-export default function Profile() {
-    return (
-        <View>
-            <LinearGradient 
-            colors={['#414066', '#91BDC0', '#91BDC0']}
-            locations = {[0, 0.9, 1]}
-            start = {{ x: 0, y: 0 }}
-            end = {{ x: 1, y: 1 }}>
-                <View style = {styles.background}>
-                        <Text style = {styles.postText}>Profile Page</Text>                     
-                </View>
-            </LinearGradient>
-        </View>
-    )
+export default class Profile extends Component {
+    render(){
+        return (
+            <View style={styles.container}>
+                
+                <Header />
+                <Bar />
+                <PhotoGrid />
+
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#000',
+    }
 
-    background: {
-        width: '100%',
-        height: '100%',
-        flexWrap: 'wrap',
+});
 
-        //background: linear-gradient(155.53deg, rgba(65, 64, 102, 0.66) 10.63%, #91BDC0 93.38%);
-    },
-    postText: {
-
-        position: 'absolute',
-        width: 344,
-        height: 127,
-        left: 20,
-        top: 107,
-        fontFamily: 'Montserrat-Bold',
-        fontSize: 31,
-        color: '#FFFFFF',
-    }, 
-
-
-})
-
+AppRegistry.registerComponent('Profile',() => Profile);
