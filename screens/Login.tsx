4408d43851a1d69e11230b0d25305274a85477d2
tkmoney11 <tylerkim@outlook.com>
2020-09-13 12:59:33 -0700

    Changed LoginPage to functional compenents

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,65 +1,61 @@
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
-import { useNavigation } from '@react-navigation/native';
+import { RouteStackParamList } from '../navigation/RouteParameterList';
+// 
+function LoginPage({navigation, route}: RouteStackParamList<"LoginPage">) {
+  // state={
+  //   username:"",
+  //   password:""
+  // }
 
-
-
-class LoginPage extends React.Component {
-  state={
-    username:"",
-    password:""
-  }
-  render(){
-    return (
-      <View style={styles.container}>
-        <LinearGradient
-          colors={['#726984', '#EE736A']}
-          style={{
-            position: 'absolute',
-            left: 0,
-            right: 0,
-            top: 0,
-            height: 600,
-          }}
-        />
-       
-
-        <Text style={styles.logo}>Welcome back, {"\n"} Name</Text>
-        <View style={styles.inputView} >
-          <TextInput  
-            style={styles.inputText}
-            placeholder="Username" 
-            placeholderTextColor="white"
-            onChangeText={text => this.setState({username:text})}/>
-        </View>
-        <View style={styles.underline}>
-        </View>
-        <View style={styles.inputView} >
-          <TextInput  
-            secureTextEntry
-            style={styles.inputText}
-            placeholder="Password" 
-            placeholderTextColor="white"
-            onChangeText={text => this.setState({password:text})}/>
-        </View>
-        <View style={styles.underline}>
-        </View>
-        <TouchableOpacity style={styles.loginBtn}>
-          <Text style={styles.loginText}>Log In</Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Text style={styles.forgot}>Forgot your password?</Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Text style={styles.signUpText}>Not your account?{"\n"}Sign Up</Text>
-          {/* <GoToButton screenName="Signup"/> */}
-        </TouchableOpacity>
-
-      </View>
+  return (
+    <View style={styles.container}>
+      <LinearGradient
+        colors={['#726984', '#EE736A']}
+        style={{
+          position: 'absolute',
+          left: 0,
+          right: 0,
+          top: 0,
+          height: 600,
+        }}
+      />
       
-    );
-  }
+
+      <Text style={styles.logo}>Welcome back, {"\n"} Name</Text>
+      <View style={styles.inputView} >
+        <TextInput  
+          style={styles.inputText}
+          placeholder="Username" 
+          placeholderTextColor="white"
+          onChangeText={text => console.log("tbd, setstate for username")}/>
+      </View>
+      <View style={styles.underline}>
+      </View>
+      <View style={styles.inputView} >
+        <TextInput  
+          secureTextEntry
+          style={styles.inputText}
+          placeholder="Password" 
+          placeholderTextColor="white"
+          onChangeText={text => console.log("tbd, setstate for password")}/>
+      </View>
+      <View style={styles.underline}>
+      </View>
+      <TouchableOpacity style={styles.loginBtn}>
+        <Text style={styles.loginText}>Log In</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text style={styles.forgot}>Forgot your password?</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate('SignUpPage')}>
+        <Text style={styles.signUpText}>Not your account?{"\n"}Sign Up</Text>
+      </TouchableOpacity>
+
+    </View>
+    
+  );
 }
 
 export default LoginPage;
